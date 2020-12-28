ecef607b79dcb91e668f162e525c64d017fcee50
Ro5hi <slen.detablan@gmail.com>
2020-12-28 13:14:42 -0700

    added print loggers and else statement

--- BeerSong.java
+++ BeerSong.java
@@ -7,6 +7,17 @@ public class BeerSong {
             if (beerNum == 1) {
                 word = "bottle";
             }
-        }
-    }
-}
+            System.out.println(beerNum + "" + word + " of beer on the wall");
+            System.out.println(beerNum + "" + word + "of beer");
+            System.out.println("Take one down");
+            System.out.println("Pass it around");
+            beerNum = beerNum - 1;
+
+            if (beerNum > 0) {
+                System.out.println(beerNum + "" + word + " of beer on the wall");
+            } else {
+                System.out.println("No more bottles of beer on the wall");
+            } // ends else statement
+        } // ends while loop
+    } // ends main method
+} // ends class
