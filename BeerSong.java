f49611910d7bf96fd307c502fce11d06f43c8383
Ro5hi <slen.detablan@gmail.com>
2020-12-28 13:10:42 -0700

    made string arguments and while/if statement

--- BeerSong.java
+++ BeerSong.java
@@ -1,3 +1,12 @@
 public class BeerSong {
-    
+    public static void main (String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while (beerNum > 0) {
+            if (beerNum == 1) {
+                word = "bottle";
+            }
+        }
+    }
 }
