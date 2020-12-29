6dfcf00ab0f45b013976dc52a27039ea0f618375
Ro5hi <slen.detablan@gmail.com>
2020-12-29 13:23:13 -0700

    added IfTest2

---
+++ IfTest2.java
@@ -0,0 +1,20 @@
+class IfTest2 {
+    public static void main(String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        } else {
+            System.out.println("x is NOT 3");
+        }
+        System.out.println("This ruuns no matter what");
+    }
+}
+
+// java IfTest2
+// x is NOT 3
+// This runs no matter what
+
+/* 
+    We can add an else to the condition so that we can say something like, "If there's still beer,
+    keep coding, else (otherwise) get more beer, and then continue on..." .
+*/
