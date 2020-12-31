a5e056ca436d090a45cd2372bcf7170e9cc10895
Ro5hi <slen.detablan@gmail.com>
2020-12-31 14:34:56 -0700

    added shuffle exercise, output has to be a-b c-d

---
+++ Shuffle1.java
@@ -0,0 +1,25 @@
+// Output has to be 
+// a-b c-d
+
+public class Shuffle1 {
+    public static void main(String [] args) {
+        int x = 3;
+        while (x > 0) {
+            if (x == 2) {
+                System.out.print("b c");
+            }
+            if (x > 2) {
+                System.out.print("a");
+            }
+            if (x == 1) {
+                System.out.print("d");
+                x = x - 1;
+            }
+            x = x - 1;
+            System.out.print("-");
+        }
+    }
+}
+
+// javac Shuffle1
+// java Shuffle1
