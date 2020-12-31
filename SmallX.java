67f1038d1d65282e610b8bc6f3099cccfbfb78cd
Ro5hi <slen.detablan@gmail.com>
2020-12-31 15:16:15 -0700

    added public class to compile and print "small x"

--- SmallX.java
+++ SmallX.java
@@ -11,3 +11,20 @@
 Needs to compile and print "Small x"
 
 */
+
+public class SmallX {
+    public static void main (String [] args) {
+        int x = 5;
+        while ( x > 1) {
+            x = x - 1;
+            if (x < 3) {
+                System.out.println("Small X");
+            }
+        }
+    }
+}
+
+
+// javac SmallX
+// java SmallX
+// "Small X"
