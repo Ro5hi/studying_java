60af45f01200a7bf5ef7f6ced69029259d3ef604
Ro5hi <slen.detablan@gmail.com>
2020-12-31 15:12:05 -0700

    exercise1b compiler needs to compile and print big x

---
+++ Exercise1b.java
@@ -0,0 +1,15 @@
+/* 
+
+Exercise1b won't compile
+
+public class Exercise1b {
+    public static void main(String [] args) {
+        int x = 1;
+        while (x < 10 ) {
+            if (x > 3) {
+                System.out.println("big x");;
+            }
+        }
+    }
+}
+*/ 
