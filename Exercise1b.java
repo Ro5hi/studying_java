cf376b52e82760b9238756be37f2db96c2ce6a98
Ro5hi <slen.detablan@gmail.com>
2020-12-31 15:12:31 -0700

    added if statements to compile and print Big x

--- Exercise1b.java
+++ Exercise1b.java
@@ -13,3 +13,31 @@ public class Exercise1b {
     }
 }
 */ 
+
+
+public class Exercise1b {
+    public static void main(String [] args) {
+        int x = 1;
+        while (x < 10) {
+            if (x > 3) {
+                System.out.println("3");
+            }
+            if (x > 2) {
+                System.out.println("2");
+            }
+            if (x > 1) {
+                System.out.println("1");
+            }
+            if (x == 1) {
+                System.out.println("x is 1");
+            }
+            x = x + 9;
+            System.out.println("Big x");
+        }
+    }
+}
+
+// javac Exercise1b
+// java Exercise1b
+// x is 1 
+// Big x
