70012b63408403f04833cd9d0f0e89c864e60c54
Ro5hi <slen.detablan@gmail.com>
2021-01-04 12:57:47 -0700

    added while(true), made Player class GameLauncher class

--- Chapter2/GuessGame.java
+++ Chapter2/GuessGame.java
@@ -1,3 +1,5 @@
+package Chapter2;
+
 public class GuessGame {
     Player p1;
     Player p2;
@@ -20,7 +22,53 @@ public class GuessGame {
         boolean p3isRight = false; 
         // declare 3 variables to hold a true or false based on player's answer
 
-        
+        int targetNumber = (int) (Math.random() * 10); 
+        System.out.println("I'm thinking of a number between 0 and 9..."); 
+        // make a 'target' number that the players have to guess
+
+        while(true) {
+            System.out.println("Number to guess is " + targetNumber);
+
+            p1.guess();
+            p2.guess();
+            p3.guess();
+            // call each player's guess() method
+
+            guessp1 = p1.number;
+            System.out.println("Player one guessed " + guessp1);
+
+            guessp2 = p2.number;
+            System.out.println("Player two guessed " + guessp2);
+
+            guessp3 = p3.number;
+            System.out.println("Player three guessed " + guessp3);
+
+            if (guessp1 == targetNumber) {
+                p1isRight = true;
+            }
+            if (guessp2 == targetNumber) {
+                p2isRight = true;
+            }
+            if (guessp3 == targetNumber) {
+                p3isRight = true;
+            }
+            /*check each guess to see if it matches, if right then set player's variable
+            to be true (set to false by default) */
+
+            if (p1isRight || p2isRight || p3isRight) {
+            // if p1 or p2 or p3 is right 
+
+                System.out.println("We have a winner!");
+                System.out.println("Player one got it right!" + p1isRight);
+                System.out.println("Player two got it right!" + p2isRight);
+                System.out.println("Player three got it right!" + p3isRight);
+                System.out.println("Game is over.");
+                break;
+            } else {
+                System.out.println("Players will have to try again.");
+                // otherwise stay in the loop and ask players for another guess
+            }
+        }
     }
 
 }
