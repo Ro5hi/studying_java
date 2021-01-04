4a5cf2f8488bed8051b1ac32618b2ddc9bd4906d
Ro5hi <slen.detablan@gmail.com>
2021-01-04 12:39:23 -0700

    GuessGame, 3 players, 3 guesses, 3 booleans

--- Chapter2/01_chap2notes.md
+++ Chapter2/01_chap2notes.md
@@ -10,3 +10,8 @@
 
 - Methods, when you design a class you think about the data an object will need to know about itself, and you also design the methods that operate on that data. 
 
+**The two uses of main**
+- to test your real class
+- to launch your Java application
+
+As long as you're in main(), you're not really in OOP. It's fine for a test program to run within the main method, but in a true OO application, you need objects talking to each other, as opposed to a static main() method crating and testing objects. A real Java application is nothing but objects talking to other objects, talking means objects callig methods on one another. 
---
+++ Chapter2/GuessGame.java
@@ -0,0 +1,27 @@
+public class GuessGame {
+    Player p1;
+    Player p2;
+    Player p3;
+    // GuessGame has three instance variables for the three player objects
+
+    public void startGame() {
+        p1 = new Player();
+        p2 = new Player();
+        p3 = new Player();
+        // create three player objects and assign them to three player instance variables
+
+        int guessp1 = 0;
+        int guessp2 = 0;
+        int guessp3 = 0;
+        // declare 3 variables to hold the three guesses the players make
+
+        boolean p1isRight = false;
+        boolean p2isRight = false;
+        boolean p3isRight = false; 
+        // declare 3 variables to hold a true or false based on player's answer
+
+        
+    }
+
+}
+
