6f277b24f61b9058fef7e623261cf0c407619a26
Ro5hi <slen.detablan@gmail.com>
2021-01-05 12:59:13 -0700

    compiled TapeDeck, defined t in testdrive

--- Chapter2/TapeDeck.java
+++ Chapter2/TapeDeck.java
@@ -1,26 +1,13 @@
 package Chapter2;
 
-public class TapeDeck {
-   TapeDeck t;
+class TapeDeck {
 
-   boolean canRecord = false;
-   
-   public void playTape() {
-       System.out.println("*tape playing*");
-   }
-   public void recordTape() {
-       System.out.println("*tape recording*");
-   }
+    boolean canRecord = false;
 
-   class TapeDeckTestDrive {
-       public static void main(String [] args) {
-
-        t.canRecord = true;
-        t.playTape();
-
-        if (t.canRecord == true) {
-            t.recordTape = true;
-        }
-       }
-   }
+    void playTape() {
+        System.out.println("tape playing");
+    }
+    void recordTape() {
+        System.out.println("tape recording");
+    }
 }
