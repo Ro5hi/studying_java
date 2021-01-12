d773cf20d921b8364661bd6901765ff98ff5aca5
Ro5hi <slen.detablan@gmail.com>
2021-01-12 13:27:12 -0700

    changed array to start at 0 to compile, line 10-11

--- Chapter3/Hobbits.java
+++ Chapter3/Hobbits.java
@@ -7,20 +7,19 @@ class Hobbits {
 
         Hobbits [] h = new Hobbits[3];
 
-        int z = 0;
-
-        while (z < 3) {
+        int z = -1;
+        while (z < 2) {
             z = z + 1;
             h[z] = new Hobbits();
-            h[z].name = "bilbo";
+            h[z].name = "Bilbo";
             if (z == 1) {
-                h[z].name = "frodo";
+                h[z].name = "Frodo";
             }
             if  (z == 2) {
-                h[z].name = "sam";
+                h[z].name = "Sam";
             }
             System.out.print(h[z].name + " is a ");
-            System.out.print("good Hobbit name!");
+            System.out.print("good Hobbit name! ");
         }
     }
 }
